cb9f998456427877f97f8af1a8f2aca6a740582c
Carlos <calcontacto@gmail.com>
2023-11-29 15:20:57 +0100

    web server change

--- playwright.config.js
+++ playwright.config.js
@@ -83,6 +83,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
+    timeout: 120 * 1000,
   },
 })
